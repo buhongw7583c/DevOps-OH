87d24df56286b8e53fcfe1a54c1c52b87a8deb57
Wenjun Zhou <wez@microsoft.com>
2019-08-16 15:42:38 +0800

    update POI-CD-Ch4

--- apis/poi/web/Models/Healthcheck.cs
+++ apis/poi/web/Models/Healthcheck.cs
@@ -8,7 +8,7 @@ namespace poi.Models
         public Healthcheck()
         {
             Message = "POI Service Healthcheck";
-            Status = "Very Healthy!";
+            Status = "Very Healthy";
         }
         [Newtonsoft.Json.JsonProperty(PropertyName = "message")]
         public string Message {get;set;}
